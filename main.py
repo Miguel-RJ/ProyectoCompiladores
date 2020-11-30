5314d9fc5d6e48ef6a46035d62fbdcd9a86c9d57
Axel Frías <axel.frias257@gmail.com>
2020-11-29 23:58:07 -0600

    Todo bien

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+#Re: librería para manejo de expresiones regulares
+#Sys: Librería para manejo de sistema
 import re
 import sys
 
-#Dictionary with the regular expresions of SQL Sentences
+#Diccionario con expresiones regulares de sentencias SQL.
+#Keys -> Nombre de SQL SENTENCE
+#Values -> Expresión regular de SQL SENTENCE
 regexDictionary = {
     "create database" : r"^(CREATE DATABASE|create database) ([A-Za-z][\w]*);?",
     "use" : r"^(USE|use) ([A-Za-z][\w]*);?",
@@ -12,14 +16,22 @@ regexDictionary = {
     "update" : r"^(UPDATE|update) ([A-Za-z]([\w])*|\*) (SET|set) ([A-Za-z]([\w])*) = (null|true|false|\d+|\'[ \w]+\')(, ?([A-Za-z]([\w])*) = (null|true|false|\d+|\'[ \w]+\'))?( (where|WHERE) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\')( (AND|and) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\'))*)?;?"
 }
 
+#inyection une los argumentos enviados a través de línea de comandos
 inyection = " ".join(sys.argv[1:])
+#bandera isSqlSentence que cambia en caso de que se inserte una sentencia SQL
+isSqlSentence = False
 
+#Se analiza el valor de cada llave del diccionario, comparándolo con el valor ingresado desde la consola. 
+#Si existe un resultado, se indica el tipo de sentencia y su valor.
 for keys, values in regexDictionary.items():
     result = re.search(values, inyection)
     if result != None:
         isSqlSentence = True
         print(f"Alert! Someone wanted to insert a SQL sentence : {keys}\nThe operation was: {result.group()}, but your favorite program stopped the attack. :)")
+        break
 
-
+#Si nunca se encontró alguna coincidencia, se da por hecho que no existe riesgo.
+if not isSqlSentence:
+    print("Everything is ok!")
 
 
