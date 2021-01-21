41a7e72e1d05510e5c042a95f510dbf4a51662b4
Axel Frías Hernández <axel.frias257@gmail.com>
2021-01-20 23:23:30 -0600

    Vista parcial App Flask

---
+++ App.py
@@ -0,0 +1,116 @@
+from flask import Flask, render_template, request, flash, redirect, url_for, session
+from main import test
+import os
+import sqlite3
+
+app = Flask(__name__)
+app.secret_key = 'mySecretKey'
+
+@app.route('/')
+def index():
+    if len(os.listdir('databases')) != 0:
+        if 'db_in_use' in session:
+            if os.path.exists(f'databases/{session["db_in_use"]}'):
+                conn = sqlite3.connect(f'databases/{session["db_in_use"]}')
+                cursor = conn.cursor()
+                rows = cursor.execute(r"SELECT name FROM sqlite_master WHERE type = 'table' AND name NOT LIKE 'sqlite_%'")
+                tables = rows.fetchall()
+                
+                database_content = {}
+                for table in tables:
+                    rows_columns = cursor.execute(f'PRAGMA table_info({table[0]})')
+                    if table[0] not in database_content:
+                        database_content[table[0]] = rows_columns.fetchall()
+                if 'select_result' in session:
+                    print(session['select_result'])
+                    return render_template('index.html', available_databases = os.listdir('databases'), available_tables = database_content, db_in_use = session["db_in_use"], selected_items = session['select_result'])    
+                return render_template('index.html', available_databases = os.listdir('databases'), available_tables = database_content, db_in_use = session["db_in_use"])
+            return render_template('index.html', available_databases = os.listdir('databases'))
+        return render_template('index.html', available_databases = os.listdir('databases'))
+    return render_template('index.html')
+
+@app.route('/execute_command', methods=['POST'])
+def execute_command():
+    if request.method == 'POST':
+        command = request.form['command']
+        result = test(command, True)
+        if result != None:
+            op, flash_message = run_command(result)
+            flash(flash_message)
+            return redirect(url_for('index'))
+        return "Sin respuesta"
+
+def run_command(result):
+    if result[0] == "create database":
+        if os.path.exists('databases/'+ result[1][15:].strip() + '.db'):
+            return 1, f"Ya existe la Base de Datos {result[1][15:].strip()}"
+        sqlite3.connect('databases/'+ result[1][15:].strip() + '.db')
+        return 1, f"Base de datos {result[1][15:]} creada con éxito"
+    
+    elif result[0] == "create table":
+        if 'db_in_use' in session:
+            if os.path.exists(f'databases/{session["db_in_use"]}'):
+                conn = sqlite3.connect(f'databases/{session["db_in_use"]}')
+                cursor = conn.cursor()
+                cursor.execute(result[1])
+                return 2, "Tabla " + result[1].split(' ')[2] + " creada"
+            return 2, "No existe la Base de Datos"
+        return 2, "Utiliza una base de datos"
+    
+    elif result[0] == "use":
+        if 'db_in_use' in session:
+            session.pop(session['db_in_use'], None)
+        session['db_in_use'] = result[1][4:] + '.db'
+        return 3, "Usando " + result[1][4:]
+   
+    elif result[0] == "insert":
+        if 'db_in_use' in session:
+            if os.path.exists(f'databases/{session["db_in_use"]}'):
+                conn = sqlite3.connect(f'databases/{session["db_in_use"]}')
+                cursor = conn.cursor()
+                cursor.execute(result[1])
+                conn.commit()
+                return 4, "Registros insertados en " + result[1].split(' ')[2]
+            return 4, "No existe la Base de Datos"
+        return 4, "Selecciona una Base de Datos"
+    
+    elif result[0] == "select":
+        if 'db_in_use' in session:
+            if os.path.exists(f'databases/{session["db_in_use"]}'):
+                conn = sqlite3.connect(f'databases/{session["db_in_use"]}')
+                cursor = conn.cursor()
+                rows = cursor.execute(result[1])
+                result = rows.fetchall()
+                print(len(result))
+                if len(result) != 0:
+                    session['select_result'] = result
+                    print(session['select_result'])
+                    return 5, "Registros: "
+                return 5, "No hay registros"
+            return 5, "No existe la Base de Datos"
+        return 5, "Selecciona una Base de Datos"
+    
+    elif result[0] == "delete":
+        if 'db_in_use' in session:
+            if os.path.exists(f'databases/{session["db_in_use"]}'):
+                conn = sqlite3.connect(f'databases/{session["db_in_use"]}')
+                cursor = conn.cursor()
+                cursor.execute(result[1])
+                conn.commit()
+                return 4, "Registros eliminados"
+            return 4, "No existe la Base de Datos"
+        return 6, "Selecciona una Base de Datos"
+    
+    elif result[0] == "update":
+        if 'db_in_use' in session:
+            if os.path.exists(f'databases/{session["db_in_use"]}'):
+                conn = sqlite3.connect(f'databases/{session["db_in_use"]}')
+                cursor = conn.cursor()
+                cursor.execute(result[1])
+                conn.commit()
+                return 4, "Registro actualizado"
+            return 4, "No existe la Base de Datos"
+        return 7, "Selecciona una Base de Datos"
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 #Re: librería para manejo de expresiones regulares
 #Sys: Librería para manejo de sistema
 import re
-import sys
+#import sys
 
 #Diccionario con expresiones regulares de sentencias SQL.
 #Keys -> Nombre de SQL SENTENCE
 #Values -> Expresión regular de SQL SENTENCE
-regexDictionary = {
+regex_dictionary = {
     "create database" : r"^(CREATE DATABASE|create database) ([A-Za-z][\w]*);?",
     "use" : r"^(USE|use) ([A-Za-z][\w]*);?",
-    "create table" : r"^(CREATE TABLE|create table) [A-Za-z][\w]*(\([A-Za-z][\w]* (varchar\(\d+\)|int|bit)(,[A-Za-z][\w]* (varchar\(\d+\)|int|bit))*)\);?",
+    "create table" : r"^(CREATE TABLE|create table) [A-Za-z][\w]* (\([A-Za-z][\w]* (varchar\(\d+\)|int|bit)(, [A-Za-z][\w]* (varchar\(\d+\)|int|bit))*)\);?",
     "insert" : r"^(INSERT INTO|insert into) [A-Za-z][\w]* (VALUES|values) \((null|true|false|\d+|\'[ \w]+\')((,(null|true|false|\d+|\'[ \w]+\')))*\);?",
-    "select" : r"^(SELECT|select) ([A-Za-z]([\w])*|\*) (FROM|from) ([A-Za-z]([\w])*)( (where|WHERE) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\')( (AND|and) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\'))*)?;?",
+    "select" : r"^(SELECT|select) ([A-Za-z]([\w]*)(, [A-Za-z][\w]*)*|\*) (FROM|from) ([A-Za-z]([\w])*)( (where|WHERE) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\')( (AND|and) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\'))*)?;?",
     "delete" : r"^(DELETE|delete) (FROM|from) ([A-Za-z]([\w])*)( (where|WHERE) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\')( (AND|and) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\'))*)?;?",
-    "update" : r"^(UPDATE|update) ([A-Za-z]([\w])*|\*) (SET|set) ([A-Za-z]([\w])*) = (null|true|false|\d+|\'[ \w]+\')(, ?([A-Za-z]([\w])*) = (null|true|false|\d+|\'[ \w]+\'))?( (where|WHERE) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\')( (AND|and) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\'))*)?;?"
+    "update" : r"^(UPDATE|update) ([A-Za-z]([\w]*)(, [A-Za-z][\w]*)*|\*) (SET|set) ([A-Za-z]([\w])*) = (null|true|false|\d+|\'[ \w]+\')(, ?([A-Za-z]([\w])*) = (null|true|false|\d+|\'[ \w]+\'))?( (where|WHERE) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\')( (AND|and) ([A-Za-z]([\w]*)) = (null|true|false|\d+|\'[ \w]+\'))*)?;?"
 }
 
 #inyection une los argumentos enviados a través de línea de comandos
-inyection = " ".join(sys.argv[1:])
+#inyection = " ".join(sys.argv[1:])
 #bandera isSqlSentence que cambia en caso de que se inserte una sentencia SQL
-isSqlSentence = False
-
 #Se analiza el valor de cada llave del diccionario, comparándolo con el valor ingresado desde la consola. 
 #Si existe un resultado, se indica el tipo de sentencia y su valor.
-for keys, values in regexDictionary.items():
-    result = re.search(values, inyection)
-    if result != None:
-        isSqlSentence = True
-        print(f"Alert! Someone wanted to insert a SQL sentence : {keys}\nThe operation was: {result.group()}, but your favorite program stopped the attack. :)")
-        break
-
-#Si nunca se encontró alguna coincidencia, se da por hecho que no existe riesgo.
-if not isSqlSentence:
-    print("Everything is ok!")
+def test(sentence, operation):
+    for key, value in regex_dictionary.items():
+        result = re.search(value, sentence)
+        #If operation is true, then the code evalu
+        if result != None and operation is True:
+            #print(f"Alert! Someone wanted to insert a SQL sentence : {key}\nThe operation was: {result.group()}, but your favorite program stopped the attack. :)")
+            return key, result.group()
+        if result != None and operation is False:
+            #print(f"You're executing a SQL sentence : {key}\nThe operation was: {result.group()} :)")
+            return key, result.group()
+    return None
 
 
